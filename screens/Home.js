775d90e319cee22673497616d8ba66beb5c35ec9
fuso <fuso93@naver.com>
2022-03-07 21:36:25 +0900

    network & parsing test

--- screens/Home.js
+++ screens/Home.js
@@ -1,10 +1,39 @@
-import React from 'react';
-import {View, Text} from "react-native";
+import React, {useState} from 'react';
+import {View, Text, Button, ScrollView } from "react-native";
+import axios from "axios";
 
 const Home = () => {
+
+    const [posts, setPosts] = useState([]);
+
+
+    const getPosts = async () => {
+
+        try {
+            const {data, status} = await axios.get("https://jsonplaceholder.typicode.com/posts")
+            console.log("*********",status)
+            setPosts(data)
+
+        } catch (err){
+            alert(err.response.data.message)
+        }
+    }
     return (
         <View>
             <Text>HOME</Text>
+            <Button
+                onPress={() => getPosts()}
+                title={"데이터 가져오기"}
+                color={"purple"}
+            />
+           <ScrollView>
+               {posts.map(post => (
+                   <View style={{margin: 10, marginBottom:20}}>
+                   <Text> {post.title} </Text>
+                   <Text> {post.body} </Text>
+                   </View>
+               ))}
+           </ScrollView>
         </View>
     );
 };
